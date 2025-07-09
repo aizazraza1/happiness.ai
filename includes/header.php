692035f643331bd07d1306e04dee9b70203a6549
Aizaz Raza <aizazchouhan583@gmail.com>
2025-07-09 22:18:34 +0500

    happinessai

---
+++ includes/header.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <title>Happiness AI Home </title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="author" content="ThemeZaa">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <meta name="description" content="Happiness AI - We believe happiness is a human right. Everything we do is to make our world happier">
+        <!-- favicon icon -->
+        <link rel="shortcut icon" href="images/favicon.svg">
+        <link rel="apple-touch-icon" href="images/favicon.svg">
+        <link rel="apple-touch-icon" sizes="72x72" href="images/favicon.svg">
+        <link rel="apple-touch-icon" sizes="114x114" href="images/favicon.svg">
+        <!-- google fonts preconnect -->
+        <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <!-- style sheets and font icons  -->
+        <link rel="stylesheet" href="../css/vendors.min.css"/>
+        <link rel="stylesheet" href="../css/icon.min.css"/>
+        <link rel="stylesheet" href="../css/style.css"/>
+        <link rel="stylesheet" href="../css/responsive.css"/>
+        <link rel="stylesheet" href="../demos/application/application.css" />
+    </head>
+    <body data-mobile-nav-style="classic" class="custom-cursor"> 
+        <!-- start cursor -->
+        <div class="cursor-page-inner">
+            <div class="circle-cursor circle-cursor-inner"></div>
+            <div class="circle-cursor circle-cursor-outer"></div>
+        </div>
+        <!-- end cursor -->
+        <!-- start header -->
+        <header>
+            <!-- start navigation -->
+            <nav class="navbar navbar-expand-lg header-light bg-transparent disable-fixed" data-header-hover="light"> 
+                <div class="container-fluid">
+                    <div class="col-auto me-auto">
+                        <a class="navbar-brand" href="https://happinessai.com/">
+                            <img src="images/icon_0.svg" data-at2x="images/icon_0.svg" alt="" class="default-logo">
+                            <img src="images/icon_0.svg" data-at2x="images/icon_0.svg" alt="" class="alt-logo">
+                            <img src="images/icon_0.svg" data-at2x="images/icon_0.svg" alt="" class="mobile-logo">
+                        </a>
+                    </div>
+                    <div class="col-auto menu-order position-static">
+                        <button class="navbar-toggler float-start" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-line"></span>
+                            <span class="navbar-toggler-line"></span>
+                            <span class="navbar-toggler-line"></span>
+                            <span class="navbar-toggler-line"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav"> 
+                            <ul class="navbar-nav"> 
+                                <li class="nav-item"><a href="https://happinessai.com/" class="nav-link">home</a></li>
+                                <li class="nav-item"><a href="/about" class="nav-link">about</a></li>
+                                <li class="nav-item dropdown submenu">
+                                    <a href="/features" class="nav-link">features</a>
+                                    <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                                    <div class="dropdown-menu submenu-content" aria-labelledby="navbarDropdownMenuLink"> 
+                                        <div class="d-lg-flex mega-menu m-auto">
+                                            <ul class="col-12 col-lg-4">
+                                                <li class="w-100 p-0">
+                                                    <div class="w-100 icon-with-text-style-04 transition-inner-all md-mb-15px">
+                                                        <div class="feature-box bg-white justify-content-start h-100 border-radius-6px p-14 lg-p-13 box-shadow-quadruple-large box-shadow-quadruple-large-hover">
+                                                            <div class="feature-box-icon mb-30px"> 
+                                                                <img src="images/icon-1.webp" class="w-60px" alt="">
+                                                            </div>
+                                                            <div class="feature-box-content last-paragraph-no-margin">
+                                                                <span class="d-inline-block text-dark-gray fw-700 fs-18 mb-5px">privacy protection</span>
+                                                                <p>Your data and emotions are safeguarded to ensure a secure experience.</p>
+                                                            </div>
+                                                        </div>  
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                            <ul class="col-12 col-lg-4">
+                                                <li class="w-100 p-0">
+                                                    <div class="w-100 icon-with-text-style-04 transition-inner-all md-mb-15px">
+                                                        <div class="feature-box bg-white justify-content-start h-100 border-radius-6px p-14 lg-p-13 box-shadow-quadruple-large box-shadow-quadruple-large-hover">
+                                                            <div class="feature-box-icon mb-30px"> 
+                                                                <img src="images/icon-2.webp" class="w-60px" alt="">
+                                                            </div>
+                                                            <div class="feature-box-content last-paragraph-no-margin">
+                                                                <span class="d-inline-block text-dark-gray fw-700 fs-18 mb-5px">live emotion analysis</span>
+                                                                <p>Use advanced emotion recognition AI to receive instant feedback on your emotional state.</p>
+                                                            </div>
+                                                            <span class="position-absolute top-25px right-25px bg-dark-gray border-radius-18px text-white fs-11 fw-700 text-uppercase ps-15px pe-15px lh-26">New</span>
+                                                        </div>  
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                            <ul class="col-12 col-lg-4">
+                                                <li class="w-100 p-0">
+                                                    <div class="w-100 icon-with-text-style-04 transition-inner-all md-mb-15px">
+                                                        <div class="feature-box bg-white justify-content-start h-100 border-radius-6px p-14 lg-p-13 box-shadow-quadruple-large box-shadow-quadruple-large-hover">
+                                                            <div class="feature-box-icon mb-30px"> 
+                                                                <img src="images/icon-3.webp" class="w-60px" alt="">
+                                                            </div>
+                                                            <div class="feature-box-content last-paragraph-no-margin">
+                                                                <span class="d-inline-block text-dark-gray fw-700 fs-18 mb-5px">share happiness</span>
+                                                                <p>Share your positive emotions with family and friends to make our world happier.</p>
+                                                            </div>
+                                                        </div>  
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <!-- <div class="d-lg-flex mega-menu m-auto">
+                                            <div class="w-100 mt-70px lg-mt-45px md-mt-35px md-mb-15px sm-mt-25px sm-mb-15px">
+                                                <div class="row row-cols-2 row-cols-lg-5 row-cols-sm-3 clients-style-06 justify-content-center">
+                                                    start client item
+                                                    <div class="col client-box text-center md-mb-30px">
+                                                        <a href="#" class="justify-content-center"><img src="images/logo-walmart.svg" class="h-35px" alt=""></a>
+                                                    </div>
+                                                    end client item
+                                                    start client item
+                                                    <div class="col client-box text-center md-mb-30px">
+                                                        <a href="#" class="justify-content-center"><img src="images/logo-logitech.svg" class="h-35px" alt=""></a>
+                                                    </div>
+                                                    end client item
+                                                    start client item
+                                                    <div class="col client-box text-center md-mb-30px">
+                                                        <a href="#" class="justify-content-center"><img src="images/logo-monday.svg" class="h-35px" alt=""></a>
+                                                    </div>
+                                                    end client item
+                                                    start client item
+                                                    <div class="col client-box text-center sm-mb-30px">
+                                                        <a href="#" class="justify-content-center"><img src="images/logo-google.svg" class="h-35px" alt=""></a>
+                                                    </div> 
+                                                    end client item
+                                                    start client item
+                                                    <div class="col client-box text-center">
+                                                        <a href="#" class="justify-content-center"><img src="images/logo-paypal.svg" class="h-35px" alt=""></a>
+                                                    </div> 
+                                                    end client item
+                                                </div>
+                                            </div>
+                                        </div>  -->
+                                    </div>
+                                </li>
+                                <li class="nav-item"><a href="/how-it-works" class="nav-link">how it works</a></li>
+                                <li class="nav-item"><a href="/reviews" class="nav-link">reviews</a></li>
+                                <li class="nav-item"><a href="/subscribe" class="nav-link">subscribe</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-auto col-auto xs-ps-0">
+                        <div class="header-icon">  
+                            <div class="header-button">
+                                <a href="/download" class="btn btn-small btn-rounded with-rounded btn-box-shadow btn-dark-gray text-uppercase-inherit">download now<span class="bg-licorice-blue text-white"><i class="feather icon-feather-arrow-right"></i></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <!-- end navigation -->
+        </header>
+        <!-- end header --> 
